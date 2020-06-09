0073df452ecd8929e33e395fd8e5cc53b3b7fb81
ribnil <ribnil>
2020-06-09 10:44:11 +0900

    argement_list.hs

--- argement_list.hs
+++ argement_list.hs
@@ -1,5 +1,8 @@
 main = do
     print $ getFirst ([0..10])
     print $ getFirst "HogeHoge"
+    print $ getSecond ([0..10])
+    print $ getSecond "FugaFuga"
 
 getFirst (f:list) = f
+getSecond (_:s:_) = s
